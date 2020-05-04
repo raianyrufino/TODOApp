b0f2df000e370e06bff6271972b22f63cd72effe
Raiany <raianyrufino12@gmail.com>
2020-05-03 22:24:13 -0300

    create header component with style

--- src/App.js
+++ src/App.js
@@ -2,44 +2,54 @@ import React from 'react';
 import './App.css';
 import Tabela from './Tabela';
 
-function App() {
+import "./styles.css";
 
-    const autores =  [
-      {
-        item: 'arroz',
-        quantidade: '1',
-        preco: '6.50'
-      },
-      {
-        item: 'feijao',
-        quantidade: '3',
-        preco: '10.0'
-      },
-      {
-        item: 'suco de uva',
-        quantidade: '1',
-        preco: '11.0'
-      }
-    ];
-  
-  /*
-  removeAutor = index =>{
-    const { autores } = this.state;
-  
-    this.setState( 
-      {
-        autores : autores.filter((autor, posAtual) => {
-          return posAtual !== index;
-        }),
-      }
-    );
-  } */
+import Header from './components/Header/index';
 
-  return (
-      <div className="App">
-        <Tabela autores = { autores }/>
-      </div>
-    );
-}
+const App = () => (
+  <div className="App">
+    <Header/>
+  </div>
+);
+
+// function App() {
+
+//     const autores =  [
+//       {
+//         item: 'arroz',
+//         quantidade: '1',
+//         preco: '6.50'
+//       },
+//       {
+//         item: 'feijao',
+//         quantidade: '3',
+//         preco: '10.0'
+//       },
+//       {
+//         item: 'suco de uva',
+//         quantidade: '1',
+//         preco: '11.0'
+//       }
+//     ];
+  
+//   /*
+//   removeAutor = index =>{
+//     const { autores } = this.state;
+  
+//     this.setState( 
+//       {
+//         autores : autores.filter((autor, posAtual) => {
+//           return posAtual !== index;
+//         }),
+//       }
+//     );
+//   } */
+
+//   return (
+//       <div className="App">
+//         <Tabela autores = { autores }/>
+//       </div>
+//     );
+// }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./styles.css";
+
+// const TableHead = () => {
+//     return (
+//     <thead>
+//         <tr>
+//             <th>Item</th>
+//             <th>Quantidade</th>
+//             <th>Preço</th>
+//         </tr>
+//     </thead>
+//     );
+// }
+
+const Header = () => <header id="main-header">TODOApp</header>
+
+export default Header;
